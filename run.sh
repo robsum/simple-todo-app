ad1d719eec56a27a732f3c7b41bf06346ee6d28d
robsum <sumara.rob@gmail.com>
2023-10-09 19:55:28 +0200

    frontend

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-#cd ${SCRIPT_DIR}/frontend && docker build -t todo-frontend .
-cd ${SCRIPT_DIR}/backend && docker build -t todo-backend .
-cd ${SCRIPT_DIR}/nginx && docker build -t todo-nginx .
-cd ${SCRIPT_DIR} && docker-compose up
-
