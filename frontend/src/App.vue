635ce70c276674c432e309acf564b90fdf53c145
robsum <sumara.rob@gmail.com>
2023-10-12 18:15:24 +0200

    frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,6 @@
   <div id=app>
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <TodoForm @todo-created="handleTodoCreated" />
     <TodoList />
   </div>
 </template>
@@ -17,25 +16,6 @@ export default {
     TodoForm,
     TodoList
   },
-  methods: {
-    async handleTodoCreated(success) {
-      console.log("handleTodoCreated() in App.vue");
-      if (success) {
-        // This method is called when a new todo is created
-        // You can perform any additional actions here if needed
-        console.log('App:handleTodoCreated():Todo created successfully');
-        // const todoListComponent = this.$children.find((child) => child.$options.name === 'TodoList');
-        // if (todoListComponent) {
-        //     todoListComponent.fetchTodos();
-        // } else {
-        //     console.error('TodoList component not found among child components.');
-        // }
-        //await this.$store.dispatch('todos/fetchTodos');
-      } else {
-        console.log('App:handleTodoCreated():Failed to create a todo');
-      }
-    }
-  }
 }
 </script>
 
