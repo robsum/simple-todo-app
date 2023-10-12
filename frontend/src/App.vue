60e124cb14058b2af9b613ebdeada7d95fc2197c
robsum <sumara.rob@gmail.com>
2023-10-12 18:25:01 +0200

    frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,13 +7,11 @@
 </template>
 
 <script>
-import TodoForm from './components/TodoForm.vue';
 import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    TodoForm,
     TodoList
   },
 }
